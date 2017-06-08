7aca7718798cf938cc6f54114b2f6ac3168d563b
geraldjglasgow <geraldjglasgow>
2017-06-08 16:55:23 -0400

    new

--- quickhull/2d_top_test.py
+++ quickhull/2d_top_test.py
@@ -1,16 +1,45 @@
 import matplotlib.pyplot as plt
-import time as t
+import random
 nil = []
 hull = []
 
 def main():
-    s = [(1.1,5.0), (2.0,5.5), (3.0,5.0), (5.0,3.0), (3.0,3.0), (7.5,4.0), (7.0,5.0),(5.0,6.0),
-         (6.0,5.5), (7.0,5.3), (6.0,3.5), (6.0,4.0), (5.0,4.0), (4.0,4.0), (3.0,4.0),(5.0,5.0),
-         (5.0,-5.0),(2.5,-2.5),(6.7, -2.5),(1.3,0.0),(7.5,0.0)]
+    #s = [(1.1,5.0), (2.0,5.5), (3.0,5.0), (5.0,3.0), (3.0,3.0), (7.5,4.0), (7.0,5.0),(5.0,6.0),
+    #     (6.0,5.5), (7.0,5.3), (6.0,3.5), (6.0,4.0), (5.0,4.0), (4.0,4.0), (3.0,4.0),(5.0,5.0),
+    #     (5.0,-5.0),(2.5,-2.5),(6.7, -2.5),(1.3,0.0),(7.5,0.0),(5.0,-3.0),(5.0, 0.0)]
     s1=[]
     s2=[]
-    p1 = [(1.0,3.0)]
-    p2 = [(8.0,3.0)]
+    s=[]
+    a = []
+    b = []
+    i=0
+    while i < 15:
+        a.append(random.uniform(0.0, 50.0))
+        b.append(random.uniform(0.0, 50.0))
+        s.append((a[i], b[i]))
+        i+=1
+
+    i=0
+    max = 0
+    min = 0
+    while i < len(s):
+        if(s[i][0] < s[min][0]):
+            min = i
+        if (s[i][0] > s[max][0]):
+            max = i
+        i+=1
+    p1 = []
+    p2 = []
+    p1.append(s[min])
+    p2.append(s[max])
+    if s[max][0] < s[min][0]:
+        del s[min]
+        del s[max]
+    else:
+        del s[max]
+        del s[min]
+
+
 
     hull.append(p2[0])
     hull.append(p1[0])
@@ -30,39 +59,25 @@ def main():
             nil.append(s[i])
         i += 1
     # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-    x3, y3 = zip(*nil)
-    x4, y4 = zip(*hull)
-    plt.scatter(x3, y3, marker='o', color='black')
-    plt.scatter(x4, y4, marker='o', color='orange')
+    if len(nil) != 0:
+        x3, y3 = zip(*nil)
+        plt.scatter(x3, y3, marker='o', color='black')
     if len(s1) != 0:
         x1, y1 = zip(*s1)
         plt.scatter(x1, y1, marker='x', color='blue')
     if len(s2) != 0:
         x2, y2 = zip(*s2)
         plt.scatter(x2, y2, marker='x', color='green')
-    plt.axis([-10, 10, -10, 10])
+    x4, y4 = zip(*hull)
+    plt.scatter(x4, y4, marker='o', color='orange')
+    plt.axis([-5, 50, -5, 50])
     plt.grid(True)
     plt.show()
     # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-
-
     split_point(s1, p1, p2, 1)
-    split_point(s2, p2, p1, -1)
-    #t.sleep(4)
-    #run2()
+    split_point(s2, p1, p2, -1)
 
-def run2():
-    s = [(1.3, 2.5), (1.7, 3.5), (2.3, 4.3), (3.0, 5.0), (3.5, 5.3), (3.5, 2.5), (4.5, 5.5),
-         (5.0, 5.5), (5.5, 2.5), (8.0, 1.3), (7.8, 2.3), (7.7, 3.2), (7.0, 4.0), (6.8, 4.8), (5.9, 5.2)]
-    p1 = [(1.0, 1.0)]
-    p2 = [(8.0, 0.5)]
-    del hull[:]
-    del nil[:]
-    plt.clf()
-    hull.append(p2[0])
-    hull.append(p1[0])
-    split_point(s, p1, p2)
 
 def split_point(s, p1, p2, pos):
     p3 = []
@@ -83,37 +98,52 @@ def split_point(s, p1, p2, pos):
         m2 = ((p3[0][1] - p2[0][1]) / (p3[0][0] - p2[0][0]))
     b1 = (-(m1 * p3[0][0]) + p3[0][1])
     b2 = (-(m2 * p2[0][0])) + p2[0][1]
+    if pos == 1:
+        while i < len(s):
+            y = (m1 * s[i][0]) + b1
+            y2 = (m2 * s[i][0]) + b2
+            if s[i][1] >= y and s[i][1] <= y2:# and s[i][0] < p3[0][0]:
+                s1.append(s[i])
+            elif s[i][1] <= y and s[i][1] >= y2:# and s[i][0] > p3[0][0]:
+                s2.append(s[i])
+            elif s[i][1] > y and s[i][1] > y2 and ((s[i][1] - y) < (s[i][1] - y2)):
+                s2.append(s[i])
+            elif s[i][1] > y and s[i][1] > y2 and ((s[i][1] - y) > (s[i][1] - y2)):
+                s1.append(s[i])
+            else:
+                nil.append(s[i])
+            i += 1
+    if pos == -1:
+        while i < len(s):
+            x1 = (s[i][1] - b1) / m1
+            x2 = (s[i][1] - b2) / m2
+            if s[i][0] < x1:
+                s1.append(s[i])
+            elif s[i][0] > x2:
+                s2.append(s[i])
+            elif s[i][0] > x1 and s[i][0] < x2:
+                nil.append(s[i])
+            i+=1
 
-    #print "slope 1: ", m1, " b1: ", b1, " slope 2: ", m2, " b2: ", b2
-    while i < len(s):
-        y = (m1 * s[i][0]) + b1
-        y2 = (m2 * s[i][0]) + b2
-        if s[i][1]* pos >= y and s[i][1]*pos <= y2:# and s[i][0] < p3[0][0]:
-            s1.append(s[i])
-        elif s[i][1]*pos <= y and s[i][1]*pos >= y2:# and s[i][0] > p3[0][0]:
-            s2.append(s[i])
-        elif s[i][1] > y and s[i][1] > y2:
-            s1.append(s[i])
-        else:
-            nil.append(s[i])
-        i += 1
-    x3, y3 = zip(*nil)
-    x4, y4 = zip(*hull)
-    plt.scatter(x3, y3, marker='o', color='black')
-    plt.scatter(x4, y4, marker='o', color='orange')
+    if len(nil) != 0:
+        x3, y3 = zip(*nil)
+        plt.scatter(x3, y3, marker='o', color='black')
+
     if len(s1) != 0:
         x1, y1 = zip(*s1)
         plt.scatter(x1, y1, marker='x', color='blue')
     if len(s2) != 0:
         x2, y2 = zip(*s2)
         plt.scatter(x2, y2, marker='x', color='green')
-    plt.axis([-10, 10, -10, 10])
+    x4, y4 = zip(*hull)
+    plt.scatter(x4, y4, marker='o', color='orange')
+    plt.axis([-5, 50, -5, 50])
     plt.grid(True)
     plt.show()
     if len(s1) != 0:
-        split_point(s1,p1,p2,pos)
+        split_point(s1,p1,p3,pos)
     if len(s2) != 0:
-        split_point(s2,p2,p3,pos)
+        split_point(s2,p3,p2,pos)
 
 def getmax(s, pos):
     i=0
