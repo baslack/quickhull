ce6ea97ea4e7bfcb21a6b27f255949fd7ba14afc
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-06-09 12:13:06 -0400

    done

--- quickhull/2DQuickHull.py
+++ quickhull/2DQuickHull.py
@@ -1,49 +1,24 @@
 
 import matplotlib.pyplot as plt
-import random
+import Helper
+
 nil = []
 hull = []
 """
 2D convex hull works fine. Just need to comment code and cleanup sections by adding function calls. 
 """
-def main():
-    #s = [(1.1,5.0), (2.0,5.5), (3.0,5.0), (5.0,3.0), (3.0,3.0), (7.5,4.0), (7.0,5.0),(5.0,6.0),
-    #     (6.0,5.5), (7.0,5.3), (6.0,3.5), (6.0,4.0), (5.0,4.0), (4.0,4.0), (3.0,4.0),(5.0,5.0),
-    #     (5.0,-5.0),(2.5,-2.5),(6.7, -2.5),(1.3,0.0),(7.5,0.0),(5.0,-3.0),(5.0, 0.0)]
-    s1=[]
-    s2=[]
-    s=[]
-    a = []
-    b = []
-    i=0
-    while i < 15:
-        a.append(random.uniform(0.0, 50.0))
-        b.append(random.uniform(0.0, 50.0))
-        s.append((a[i], b[i]))
-        i+=1
 
-    i=0
-    max = 0
-    min = 0
-    while i < len(s):
-        if (s[i][0] > s[max][0]):
-            max = i
-        i+=1
-    i=0
-    p2 = []
-    p2.append(s[max])
-    del s[max]
-    while i < len(s):
-        if(s[i][0] < s[min][0]):
-            min = i
-        i+=1
-    p1 = []
-    p1.append(s[min])
-    del s[min]
+
+def main():
+    s1 = []
+    s2 = []
+    s = Helper.get_points()
+    p1, p2, s = Helper.get_initial_points(s)
 
     hull.append(p2[0])
     hull.append(p1[0])
-    i=0
+
+    i = 0
     if (p1[0][0] - p2[0][0]) == 0:
         m1 = 0
     else:
@@ -86,15 +61,16 @@ def main():
     plt.grid(True)
     plt.show()
 
+
 def split_point(s, p1, p2, pos):
     p3 = []
-    index = getmax(s, pos)
+    index = Helper.get_max(s, pos)
     p3.append(s[index])
     del s[index]
     hull.append(p3[0])
     s1 = []
     s2 = []
-    i=0
+    i = 0
     if (p1[0][0] - p3[0][0]) == 0:
         m1 = 0
     else:
@@ -109,9 +85,9 @@ def split_point(s, p1, p2, pos):
         while i < len(s):
             y = (m1 * s[i][0]) + b1
             y2 = (m2 * s[i][0]) + b2
-            if s[i][1] >= y and s[i][1] <= y2:# and s[i][0] < p3[0][0]:
+            if s[i][1] >= y and s[i][1] <= y2:
                 s1.append(s[i])
-            elif s[i][1] <= y and s[i][1] >= y2:# and s[i][0] > p3[0][0]:
+            elif s[i][1] <= y and s[i][1] >= y2:
                 s2.append(s[i])
             elif s[i][1] > y and s[i][1] > y2 and ((s[i][1] - y) < (s[i][1] - y2)):
                 s2.append(s[i])
@@ -130,37 +106,12 @@ def split_point(s, p1, p2, pos):
                 s2.append(s[i])
             else:
                 nil.append(s[i])
-            i+=1
-    """ 
-    if len(nil) != 0:
-        x3, y3 = zip(*nil)
-        plt.scatter(x3, y3, marker='o', color='black')
-
+            i += 1
     if len(s1) != 0:
-        x1, y1 = zip(*s1)
-        plt.scatter(x1, y1, marker='x', color='blue')
+        split_point(s1, p1, p3, pos)
     if len(s2) != 0:
-        x2, y2 = zip(*s2)
-        plt.scatter(x2, y2, marker='x', color='green')
-    x4, y4 = zip(*hull)
-    plt.scatter(x4, y4, marker='o', color='orange')
-    plt.axis([-5, 50, -5, 50])
-    plt.grid(True)
-    plt.show()
-    """
-    if len(s1) != 0:
-        split_point(s1,p1,p3,pos)
-    if len(s2) != 0:
-        split_point(s2,p3,p2,pos)
+        split_point(s2, p3, p2, pos)
 
-def getmax(s, pos):
-    i=0
-    max = 0
-    while i < len(s):
-        if pos*s[i][1] > pos*s[max][1]:
-            max = i
-        i+=1
-    return max
 
 if __name__ == "__main__":
     main()
