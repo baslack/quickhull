bcfb265b81e63cd8903be694242b6159f6407f52
Bishop <jhs3920@wmich.edu>
2017-06-12 15:02:44 -0400

    working on the GUI

--- ui/GUI.py
+++ ui/GUI.py
@@ -34,49 +34,71 @@
 
 import Tkinter as tk
 
+
 class ExampleApp(tk.Frame):
-    ''' An example application for TkInter.  Instantiate
-        and call the run method to run. '''
+    """ An example application for TkInter.  Instantiate
+        and call the run method to run. """
+
     def __init__(self, master):
-        # Initialize window using the parent's constructor
         tk.Frame.__init__(self, master, width=300, height=200)
-        # Set the title
         self.master.title('Alg Project')
 
         # This allows the size specification to take effect
         self.pack_propagate(0)
-
-        # We'll use the flexible pack layout manager
         self.pack()
 
-        # The greeting selector
-        # Use a StringVar to access the selector's value
-        self.greeting_var = tk.StringVar()
-        self.greeting = tk.OptionMenu(self, self.greeting_var, 'hello', 'goodbye', 'heyo')
-        self.greeting_var.set('hello')
+        # Sets min max windows
+        # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        tk.Label(self, text="Min X = ").grid(row=0, column=0)
+        self.minX = tk.StringVar()
+        self.minXSet = tk.Entry(self, textvariable=self.minX).grid(row=0, column=1)
+        self.minX.set('0')
 
-        # The recipient text entry control and its StringVar
-        self.recipient_var = tk.StringVar()
-        self.recipient = tk.Entry(self, textvariable=self.recipient_var)
-        self.recipient_var.set('world')
+        tk.Label(self, text="Max X = ").grid(row=0, column=2)
+        self.maxX = tk.StringVar()
+        self.maxXSet = tk.Entry(self, textvariable=self.maxX).grid(row=0, column=3)
+        self.maxX.set('100000')
+
+        # YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY
+        tk.Label(self, text="Min Y = ").grid(row=1, column=0)
+        self.minY = tk.StringVar()
+        self.minYSet = tk.Entry(self, textvariable=self.minY).grid(row=1, column=1)
+        self.minY.set('0')
+
+        tk.Label(self, text="Max Y = ").grid(row=1, column=2)
+        self.maxY = tk.StringVar()
+        self.maxYSet = tk.Entry(self, textvariable=self.maxY).grid(row=1, column=3)
+        self.maxY.set('100000')
+
+        # ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+        tk.Label(self, text="Min Z = ").grid(row=2, column=0)
+        self.minZ = tk.StringVar()
+        self.minZSet = tk.Entry(self, textvariable=self.minZ).grid(row=2, column=1)
+        self.minZ.set('0')
+
+        tk.Label(self, text="Max Z = ").grid(row=2, column=2)
+        self.maxZ = tk.StringVar()
+        self.maxZSet = tk.Entry(self, textvariable=self.maxZ).grid(row=2, column=3)
+        self.maxZ.set('100000')
+
+        # Number of points #####################################
+        tk.Label(self, text="Number of points = ").grid(row=4, column=0)
+        self.numPoi = tk.StringVar()
+        self.numPoiSet = tk.Entry(self, textvariable=self.numPoi).grid(row=4, column=1)
+        self.numPoi.set('100')
 
         # The go button
-        self.go_button = tk.Button(self, text='Go', command=self.print_out)
+        self.go_button = tk.Button(self, text='Launch', command=self.startApp()).grid(row=6, column=2)
 
-        # Put the controls on the form
-        self.go_button.pack(fill=tk.X, side=tk.BOTTOM)
-        self.greeting.pack(fill=tk.X, side=tk.TOP)
-        self.recipient.pack(fill=tk.X, side=tk.TOP)
+    def startApp(self):
+        """Starts selected algorithms"""
+        print('%s, %s!' % (self.minX.get(),
+                           self.maxX.get()))
 
-    def print_out(self):
-        ''' Print a greeting constructed
-            from the selections made by
-            the user. '''
-        print('%s, %s!' % (self.greeting_var.get().title(),
-                           self.recipient_var.get()))
     def run(self):
-        ''' Run the app '''
+        """ Run the app """
         self.mainloop()
+
 
 app = ExampleApp(tk.Tk())
 app.run()
